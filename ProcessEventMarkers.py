78a95bd22f692f82034b4987ce35f40d488cb1f2
Ziheng(Leo) Li <s-vector.lee@hotmail.com>
2021-11-18 22:08:46 -0500

    add epoch script for visual search

--- ProcessEventMarkers.py
+++ ProcessEventMarkers.py
@@ -9,7 +9,7 @@ from rena.utils.data_utils import RNStream
 
 # file paths
 # data_path = 'C:/Recordings/11_17_2021_22_56_15-Exp_myexperiment-Sbj_someone-Ssn_0.dats'
-from utils import interpolate_nan_array, add_event_markers_to_data_array, plot_epochs
+from utils import interpolate_nan_array, add_event_markers_to_data_array, plot_epochs, plot_epochs_visual_search
 
 tmin = -0.1
 tmax = 3
@@ -21,14 +21,14 @@ color_dict = {'Target': 'red', 'Distractor': 'blue', 'Novelty': 'yellow'}
 # session_log_path = 'C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2021Fall/11-13-2021/ReNaSessionLog_11-13-2021-11-03-54.json'
 
 # second participant
-# data_path = 'C:/Users/S-Vec/Downloads/ReNaPilot-2021Fall/11-10-2021/11_10_2021_12_06_17-Exp_ReNaPilot-Sbj_ZL-Ssn_0.dats'
-# item_catalog_path = 'C:/Users/S-Vec/Downloads/ReNaPilot-2021Fall/11-10-2021/ReNaItemCatalog_11-10-2021-12-04-46.json'
-# session_log_path = 'C:/Users/S-Vec/Downloads/ReNaPilot-2021Fall/11-10-2021/ReNaSessionLog_11-10-2021-12-04-46.json'
+data_path = 'C:/Users/S-Vec/Downloads/ReNaPilot-2021Fall/11-10-2021/11_10_2021_12_06_17-Exp_ReNaPilot-Sbj_ZL-Ssn_0.dats'
+item_catalog_path = 'C:/Users/S-Vec/Downloads/ReNaPilot-2021Fall/11-10-2021/ReNaItemCatalog_11-10-2021-12-04-46.json'
+session_log_path = 'C:/Users/S-Vec/Downloads/ReNaPilot-2021Fall/11-10-2021/ReNaSessionLog_11-10-2021-12-04-46.json'
 
 # second participant
-data_path = 'C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2021Fall/11-10-2021/11_10_2021_12_06_17-Exp_ReNaPilot-Sbj_ZL-Ssn_0.dats'
-item_catalog_path = 'C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2021Fall/11-10-2021/ReNaItemCatalog_11-10-2021-12-04-46.json'
-session_log_path = 'C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2021Fall/11-10-2021/ReNaSessionLog_11-10-2021-12-04-46.json'
+# data_path = 'C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2021Fall/11-10-2021/11_10_2021_12_06_17-Exp_ReNaPilot-Sbj_ZL-Ssn_0.dats'
+# item_catalog_path = 'C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2021Fall/11-10-2021/ReNaItemCatalog_11-10-2021-12-04-46.json'
+# session_log_path = 'C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2021Fall/11-10-2021/ReNaSessionLog_11-10-2021-12-04-46.json'
 
 varjoEyetracking_preset_path = 'D:/PycharmProjects/RealityNavigation/Presets/LSLPresets/VarjoEyeData.json'
 
@@ -44,16 +44,27 @@ item_codes = list(item_catalog.values())
 # process data
 event_markers_rsvp = data['Unity.ReNa.EventMarkers'][0][0:4]
 event_markers_carousel = data['Unity.ReNa.EventMarkers'][0][4:8]
+event_markers_vs = data['Unity.ReNa.EventMarkers'][0][8:12]
 
 event_markers_timestamps = data['Unity.ReNa.EventMarkers'][1]
+
+itemMarkers = data['Unity.ReNa.ItemMarkers'][0]
+itemMarkers_timestamps = data['Unity.ReNa.ItemMarkers'][1]
+
 eyetracking_data = data['Unity.VarjoEyeTracking'][0]
 eyetracking_data_timestamps = data['Unity.VarjoEyeTracking'][1]
 
-epochs_rsvp = plot_epochs(event_markers_rsvp, event_markers_timestamps, eyetracking_data, eyetracking_data_timestamps,
-                          varjoEyetracking_preset['ChannelNames'], session_log,
-                          item_codes, tmin, tmax, color_dict, title=title + ' RSVP')
+# epochs_rsvp = plot_epochs(event_markers_rsvp, event_markers_timestamps, eyetracking_data, eyetracking_data_timestamps,
+#                           varjoEyetracking_preset['ChannelNames'], session_log,
+#                           item_codes, tmin, tmax, color_dict, title=title + ' RSVP')
+#
+# epochs_carousel = plot_epochs(event_markers_carousel, event_markers_timestamps, eyetracking_data,
+#                               eyetracking_data_timestamps,
+#                               varjoEyetracking_preset['ChannelNames'], session_log,
+#                               item_codes, tmin, tmax, color_dict, title=title + ' Carousel')
 
-epochs_carousel = plot_epochs(event_markers_carousel, event_markers_timestamps, eyetracking_data,
-                              eyetracking_data_timestamps,
-                              varjoEyetracking_preset['ChannelNames'], session_log,
-                              item_codes, tmin, tmax, color_dict, title=title + ' Carousel')
+plot_epochs_visual_search(itemMarkers, itemMarkers_timestamps, event_markers_vs, event_markers_timestamps,
+                          eyetracking_data,
+                          eyetracking_data_timestamps,
+                          varjoEyetracking_preset['ChannelNames'], session_log,
+                          item_codes, tmin, tmax, color_dict, title=title + ' Carousel')
--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ from mne import find_events, Epochs
 
 from rena.utils.data_utils import RNStream
 
+
 def interpolate_nan(x):
     not_nan = np.logical_not(np.isnan(x))
     indices = np.arange(len(x))
@@ -23,7 +24,9 @@ def interpolate_nan_array(data_array):
     """
     return np.array([interpolate_nan(x) for x in data_array])
 
-def add_event_markers_to_data_array(event_markers, event_marker_timestamps, data_array, data_timestamps, session_log, item_codes):
+
+def add_event_markers_to_data_array(event_markers, event_marker_timestamps, data_array, data_timestamps, session_log,
+                                    item_codes):
     block_num = None
     assert event_markers.shape[0] == 4
     data_event_marker_array = np.zeros(shape=(4, data_array.shape[1]))
@@ -49,27 +52,33 @@ def add_event_markers_to_data_array(event_markers, event_marker_timestamps, data
             elif event in novelties:
                 data_event_marker_array[0][data_event_marker_index] = 3
             data_event_marker_array[1:4, data_event_marker_index] = info1, info2, info3
-            print('    Item event {0} is novelty with info {1}'.format(event, str(data_event_marker_array[0:4, data_event_marker_index])))
+            print('    Item event {0} is novelty with info {1}'.format(event, str(data_event_marker_array[0:4,
+                                                                                  data_event_marker_index])))
 
     return np.concatenate([data_array, data_event_marker_array], axis=0), event_ids
 
 
-def plot_epochs(event_markers, event_marker_timestamps, data_array, data_timestamps, data_channel_names, session_log, item_codes, tmin, tmax, color_dict, title=''):
+def plot_epochs(event_markers, event_marker_timestamps, data_array, data_timestamps, data_channel_names, session_log,
+                item_codes, tmin, tmax, color_dict, title=''):
     # interpolate nan's
     data_array = interpolate_nan_array(data_array)
 
     srate = len(data_timestamps) / (data_timestamps[-1] - data_timestamps[0])
-    eyetracking_with_event_marker_data, event_ids = add_event_markers_to_data_array(event_markers, event_marker_timestamps,
-                                                                               data_array,
-                                                                               data_timestamps, session_log,
-                                                                               item_codes)
+    eyetracking_with_event_marker_data, event_ids = add_event_markers_to_data_array(event_markers,
+                                                                                    event_marker_timestamps,
+                                                                                    data_array,
+                                                                                    data_timestamps, session_log,
+                                                                                    item_codes)
 
-    info = mne.create_info(data_channel_names + ['EventMarker'] + ["CarouselDistance", "CarouselAngularSpeed", "CarouselIncidentAngle"], sfreq=srate,
-                           ch_types=['misc'] * len(data_channel_names) + ['stim'] + ['misc'] * 3)  #  with 3 additional info markers
+    info = mne.create_info(
+        data_channel_names + ['EventMarker'] + ["CarouselDistance", "CarouselAngularSpeed", "CarouselIncidentAngle"],
+        sfreq=srate,
+        ch_types=['misc'] * len(data_channel_names) + ['stim'] + ['misc'] * 3)  # with 3 additional info markers
     raw = mne.io.RawArray(eyetracking_with_event_marker_data, info)
 
     # pupil epochs
-    epochs = Epochs(raw, events=find_events(raw, stim_channel='EventMarker'), event_id=event_ids, tmin=tmin, tmax=tmax, baseline=(0, 0),
+    epochs = Epochs(raw, events=find_events(raw, stim_channel='EventMarker'), event_id=event_ids, tmin=tmin, tmax=tmax,
+                    baseline=(0, 0),
                     preload=True,
                     verbose=False, picks=['L Pupil Diameter', 'R Pupil Diameter'])
 
@@ -84,7 +93,8 @@ def plot_epochs(event_markers, event_marker_timestamps, data_array, data_timesta
         plt.fill_between(time_vector, y1, y2, where=y2 <= y1, facecolor=color_dict[event_name],
                          interpolate=True,
                          alpha=0.5)
-        plt.plot(time_vector, np.mean(y, axis=0), c=color_dict[event_name], label='{0}, N={1}'.format(event_name, epochs[event_name].get_data().shape[0]))
+        plt.plot(time_vector, np.mean(y, axis=0), c=color_dict[event_name],
+                 label='{0}, N={1}'.format(event_name, epochs[event_name].get_data().shape[0]))
     plt.xlabel('Time (sec)')
     plt.ylabel('Pupil Diameter (averaged left and right z-score), shades are SEM')
     plt.legend()
@@ -104,7 +114,62 @@ def plot_epochs(event_markers, event_marker_timestamps, data_array, data_timesta
     # # [plt.plot(time_vector, x) for x in y_gaze_x]
     # plt.imshow(y_gaze_x)
     # plt.show()
-
-
     return epochs
 
+
+def plot_epochs_visual_search(itemMarkers, itemMarkers_timestamps, event_markers, event_marker_timestamps, data_array, data_timestamps, data_channel_names,
+                              session_log, item_codes, tmin, tmax, color_dict, title=''):
+    # interpolate nan's
+    data_array = interpolate_nan_array(data_array)
+    srate = len(data_timestamps) / (data_timestamps[-1] - data_timestamps[0])
+    eyetracking_with_event_marker_data, event_ids = add_event_markers_to_data_array(event_markers,
+                                                                                    event_marker_timestamps,
+                                                                                    data_array,
+                                                                                    data_timestamps, session_log,
+                                                                                    item_codes)
+
+    info = mne.create_info(
+        data_channel_names + ['EventMarker'] + ["", "CarouselAngularSpeed", "CarouselIncidentAngle"],
+        sfreq=srate,
+        ch_types=['misc'] * len(data_channel_names) + ['stim'] + ['misc'] * 3)  # with 3 additional info markers
+    raw = mne.io.RawArray(eyetracking_with_event_marker_data, info)
+
+    # pupil epochs
+    epochs = Epochs(raw, events=find_events(raw, stim_channel='EventMarker'), event_id=event_ids, tmin=tmin, tmax=tmax,
+                    baseline=(0, 0),
+                    preload=True,
+                    verbose=False, picks=['L Pupil Diameter', 'R Pupil Diameter'])
+
+    for event_name, event_marker_id in event_ids.items():
+        y = epochs[event_name].get_data()
+        y = np.mean(y, axis=1)  # average left and right
+        y = scipy.stats.zscore(y, axis=1, ddof=0, nan_policy='propagate')
+
+        y1 = np.mean(y, axis=0) + scipy.stats.sem(y, axis=0)  # this is the upper envelope
+        y2 = np.mean(y, axis=0) - scipy.stats.sem(y, axis=0)  # this is the lower envelope
+        time_vector = np.linspace(tmin, tmax, y.shape[-1])
+        plt.fill_between(time_vector, y1, y2, where=y2 <= y1, facecolor=color_dict[event_name],
+                         interpolate=True,
+                         alpha=0.5)
+        plt.plot(time_vector, np.mean(y, axis=0), c=color_dict[event_name],
+                 label='{0}, N={1}'.format(event_name, epochs[event_name].get_data().shape[0]))
+    plt.xlabel('Time (sec)')
+    plt.ylabel('Pupil Diameter (averaged left and right z-score), shades are SEM')
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
+    # gaze epochs
+    # epochs = Epochs(raw, events=find_events(raw), event_id=event_ids, tmin=0, tmax=tmax, baseline=(0, 0),
+    #                 preload=True,
+    #                 verbose=False, picks=['L Gaze Direction X', 'L Gaze Direction Y', 'L Gaze Direction Z', 'R Gaze Direction X', 'R Gaze Direction Y', 'R Gaze Direction Z', "EventMarker", "CarouselDistance", "CarouselAngularSpeed", "CarouselIncidentAngle"])
+    # y = epochs['Distractor'].get_data()
+    # incidnet_angles = y[:, 9, 0]
+    # sort_indices = np.argsort(incidnet_angles, axis=0)  # sort the ERP by incident angle
+    # y = y[sort_indices]
+    # y_gaze_x = np.mean(y[:, [0, 3], :], axis=1)  # take the gaze direction x and average
+    # time_vector = np.linspace(tmin, tmax, y.shape[-1])
+    # # [plt.plot(time_vector, x) for x in y_gaze_x]
+    # plt.imshow(y_gaze_x)
+    # plt.show()
+    return epochs
