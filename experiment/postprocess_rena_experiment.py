77344d1c0dc7e30e59b62243124ae1f05804a4fc
Ziheng(Leo) Li <s-vector.lee@hotmail.com>
2022-09-28 20:51:41 -0400

    add post processing scripts

---
+++ experiment/postprocess_rena_experiment.py
@@ -0,0 +1,10 @@
+import os
+
+image_folder = 'C:/Users/Lab-User/Dropbox/ReNa/ReNaData/ReNaPilot-2022Spring/3-1-2022/0/ReNaUnityCameraCapture_03-01-2022-20-10-00'
+gaze_info_file = os.path.join(image_folder, 'GazeInfo.csv')
+
+# convert the images in to a video
+video_name = 'UnityFrames.mp4'
+
+# delete the image files
+
