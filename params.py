aaaf180415385eadc849083b7c924c6e64afd31a
Ziheng 'Leo' Li <s-vector.lee@hotmail.com>
2022-11-16 00:01:29 -0500

    add data directory in params

--- params.py
+++ params.py
@@ -5,10 +5,11 @@ import mne
 
 from utils.Bidict import Bidict
 
-base_root = "C:/Users/LLINC-Lab/Dropbox/ReNa/data/ReNaPilot-2022Fall/"
+# base_root = "C:/Users/LLINC-Lab/Dropbox/ReNa/data/ReNaPilot-2022Fall/"
 # base_root = "C:/Users/Lab-User/Dropbox/ReNa/data/ReNaPilot-2022Fall/"
-base_root = "/Users/Leo/Dropbox/ReNa/data/ReNaPilot-2022Fall"
-data_directory = "Subjects"
+# base_root = "/Users/Leo/Dropbox/ReNa/data/ReNaPilot-2022Fall"
+base_root = "D:/Dropbox/Dropbox/ReNa/data/ReNaPilot-2022Fall"
+data_directory = "Subjects-Test-IncompleteBlock"
 # data_directory = "Subjects-Test"
 # data_directory = "Subjects-Test-IncompleteBlock"
 
