update run_one_epoch to not use softmax if criterion is CELoss; fix class weight when labels are one-hot encoded

renaanalysis/multimodal/BatchIterator.py
@@ -8,11 +8,11 @@ class OrderedBatchIterator:
         # check the batch is ordered
         assert np.all(np.diff(batch_sample_indices, axis=0) > 0), "batch_sample_indices is not ordered."
         self.data_arrays = data_arrays
-        self.dataset = data_arrays[0]  # use the first array to get the number of samples
+        self.dataset = data_arrays[0][batch_sample_indices.reshape(-1)]  # use the first array to get the number of samples
         self.labels = labels
 
         # check the max sample index is no greater than the number of samples
-        assert np.max(batch_sample_indices) < len(self.dataset)
+        assert np.max(batch_sample_indices) < len(self.data_arrays[0])
 
         self.batch_sample_indices = batch_sample_indices
         self.batch_size = batch_sample_indices.shape[1]
@@ -38,9 +38,10 @@ class OrderedBatchIterator:
             self.batch_index = 0
             raise StopIteration
         this_batch_sample_indices = self.batch_sample_indices[self.batch_index]
+        self.batch_index += 1
+
         batch = [torch.Tensor(darray[this_batch_sample_indices]).to(self.device) for darray in self.data_arrays]
         batch = batch[0] if len(batch) == 1 else batch
-        self.batch_index += 1
         labels = torch.Tensor(self.labels[this_batch_sample_indices]).to(self.device)
         if len(labels.shape) == 1:
             labels = labels.unsqueeze(1)

renaanalysis/multimodal/multimodal.py
@@ -365,13 +365,38 @@ class MultiModalArrays:
             return rtn
 
     def get_class_weight(self, convert_to_tensor=False, device=None):
+        """
+        An example of one-hot encoded label array, the original labels are [0, 6]
+        The corresponding cw is:
+                     Count
+        0 -> [1, 0]  100
+        6 -> [0, 1]  200
+        cw:  [3, 1.5]
+        because pytorch treat [1, 0] as the first class and [0, 1] as the second class. However, the
+        count for unique one-hot encoded label came out of np.unique is in the reverse order [0, 1] and [1, 0].
+        the count needs to be reversed accordingly.
+
+        TODO check when adding new classes
+        @param convert_to_tensor:
+        @param device:
+        @return:
+        """
         assert self.labels_array is not None, "Class weight needs labels array but labels is not provided"
-        unique_classes, counts = np.unique(self.labels_array, return_counts=True)
+        encoded_labels = self._encoder(self.labels_array)
+        if len(encoded_labels.shape) == 2:
+            unique_classes, counts = np.unique(self._encoder(self.labels_array), return_counts=True, axis=0)
+            counts = counts[::-1]  # refer to docstring
+        elif len(encoded_labels.shape) == 1:
+            unique_classes, counts = np.unique(self._encoder(self.labels_array), return_counts=True)
+        else:
+            raise ValueError("encoded labels should be either 1d or 2d array")
+        # labels_as_strings = np.array([str(label) for label in unique_classes])
+        # counts = counts[np.flip(np.argsort(labels_as_strings))]
         class_proportions = counts / len(self.labels_array)
         class_weights = 1/class_proportions
         if convert_to_tensor:
             class_weights = torch.tensor(class_weights, dtype=torch.float32, device=device)
-        return class_weights
+        return class_weights # reverse the class weights because
 
     def encode_labels(self):
         pass  # TODO

renaanalysis/multimodal/train_multimodal.py
@@ -24,7 +24,11 @@ def train_test_classifier_multimodal(mmarray, model, test_name="", task_name=Tas
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
     criterion, last_activation = mmarray.get_label_encoder_criterion_for_model(model, device, include_metainfo=True)
-    test_dataloader = mmarray.get_test_dataloader(batch_size=batch_size, encode_y=True, return_metainfo=True, device=device)
+
+    # test_dataloader = mmarray.get_test_dataloader(batch_size=batch_size, encode_y=True, return_metainfo=True, device=device)
+
+    mmarray.training_val_test_split_ordered_by_subject_run(n_folds, batch_size=batch_size, val_size=val_size, test_size=0.1, random_seed=random_seed)
+    test_dataloader = mmarray.get_test_ordered_batch_iterator(device=device, return_metainfo=True)
 
     # X = model.prepare_data(X)
 
@@ -43,8 +47,8 @@ def train_test_classifier_multimodal(mmarray, model, test_name="", task_name=Tas
     for f_index in range(n_folds):
         model_copy = copy.deepcopy(model)
         model_copy = model_copy.to(device)
-        train_dataloader, val_dataloader = mmarray.get_dataloader_fold(f_index, batch_size=batch_size, is_rebalance_training=True,
-                                                                       random_seed=random_seed, device=device, return_metainfo=True)
+        # train_dataloader, val_dataloader = mmarray.get_dataloader_fold(f_index, batch_size=batch_size, is_rebalance_training=True,random_seed=random_seed, device=device, return_metainfo=True)
+        train_dataloader, val_dataloader = mmarray.get_train_val_ordered_batch_iterator_fold(f_index, device=device, return_metainfo=True)
 
         optimizer = torch.optim.Adam(model_copy.parameters(), lr=lr)
         # optimizer = torch.optim.SGD(model_copy.parameters(), lr=lr, momentum=0.9)
@@ -265,14 +269,14 @@ def train_test_classifier_multimodal_ordered_batches(mmarray, model, test_name="
     test_auc_folds = []
     test_acc_folds = []
     test_loss_folds = []
-    # test_iterator = mmarray.get_test_ordered_batch_iterator(device=device, return_metainfo=True)
-    test_iterator = mmarray.get_test_dataloader(batch_size=batch_size, encode_y=True, return_metainfo=True, device=device)
+    # test_iterator = mmarray.get_test_dataloader(batch_size=batch_size, encode_y=True, return_metainfo=True, device=device)
+    test_iterator = mmarray.get_test_ordered_batch_iterator(device=device, return_metainfo=True)
 
     for f_index in range(n_folds):
         model_copy = copy.deepcopy(model)
         model_copy = model_copy.to(device)
-        # train_iterator, val_iterator = mmarray.get_train_val_ordered_batch_iterator_fold(f_index, device=device, return_metainfo=True)
         train_iterator, val_iterator = mmarray.get_dataloader_fold(f_index, batch_size=batch_size, is_rebalance_training=False, random_seed=random_seed, device=device, task_name=task_name, return_metainfo=True)
+        # train_iterator, val_iterator = mmarray.get_train_val_ordered_batch_iterator_fold(f_index, device=device, return_metainfo=True)
 
         optimizer = torch.optim.Adam(model_copy.parameters(), lr=lr)
         # optimizer = torch.optim.SGD(model_copy.parameters(), lr=lr, momentum=0.9)

renaanalysis/params/params.py
@@ -169,8 +169,8 @@ eeg_epoch_ticks = np.array([0, 0.3, 0.6, 0.8])
 pupil_epoch_ticks = np.array([0, 0.5, 1., 1.5, 2., 2.5, 3])
 
 lr = 2e-4
-batch_size = 32
-epochs = 10
+batch_size = 16
+epochs = 5000
 patience = 30
 train_ratio = 0.8
 model_save_dir = 'renaanalysis/learning/saved_models'