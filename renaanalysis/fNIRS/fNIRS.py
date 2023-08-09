073857b6dcb2b4b6bdf554544e02b08af7898053
HaowenWeiJohn <hw2892@columbia.edu>
2023-08-09 23:40:47 +0800

    fnirs dataset

---
+++ renaanalysis/fNIRS/fNIRS.py
@@ -0,0 +1,16 @@
+from mat4py import loadmat
+
+
+file_path = 'D:/HaowenWei/Data/HT_Data/fNIRS/MentalArithmetic/S01.mat'
+data = loadmat(file_path)
+
+
+
+# file_data = loadmat(file_path)
+# data = file_data['data']
+#
+#
+# print('John')
+
+
+
