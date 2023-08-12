a4147ca7dffecac8155752c47b406ffde980951c
HaowenWeiJohn <hw2892@columbia.edu>
2023-08-12 13:47:28 +0800

    fnirs example dataset

--- renaanalysis/fNIRS/mne_nirs_dataset.py
+++ renaanalysis/fNIRS/mne_nirs_dataset.py
@@ -4,7 +4,6 @@ import mne_bids.stats
 import numpy as np
 
 print("fNIRS dataset example")
+datapath = mne_nirs.datasets.fnirs_motor_group.data_path()
+mne_bids.stats.count_events(datapath)
 
-data = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
-
-data_array = mne.io.RawArray(data, info)
